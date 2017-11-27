ed07a6b4e6fae1f4a861345b9443ffb36b17d748
Xiaoguang Sun <sunxiaoguang@gmail.com>
2017-11-27 13:36:40 +0800

    Print contextual information for slow command
    Print contextual information for slow command that took longer than a minute
    to finish

--- module.go
+++ module.go
@@ -11,6 +11,7 @@ import (
 	"strconv"
 	"sync"
 	"sync/atomic"
+	"time"
 )
 
 var moduleMutex sync.Mutex
@@ -19,6 +20,8 @@ var moduleInstance *redisModule = nil
 type contextKey int
 
 var sessionIdKey contextKey = 0
+
+const slowCommandThreshold = time.Minute
 
 type redisModule struct {
 	requests       []dispatcher
@@ -34,7 +37,7 @@ func (module *redisModule) downstream() dispatcher {
 	return module.downstreams[atomic.AddUint32(&module.downstreamId, 1)%module.numDownstreams]
 }
 
-func (module *redisModule) panicHandler(clientId, requestId int64, cmd []byte) {
+func (module *redisModule) commandCleanup(clientId, requestId int64, cmd []byte, startTime time.Time) {
 	if r := recover(); r != nil {
 		msg := fmt.Sprintf("Failed to dispatch request %v(%v) from client %v:%v\n%s", cmd, requestId, clientId, r, string(debug.Stack()))
 		module.logger.Print(msg)
@@ -42,12 +45,17 @@ func (module *redisModule) panicHandler(clientId, requestId int64, cmd []byte) {
 		response.WriteError("ERR Internal")
 		module.responses[clientId%int64(len(module.responses))].dispatch(response.serialize())
 	}
+	elapsed := time.Now().Sub(startTime)
+	if elapsed > slowCommandThreshold {
+		module.logger.Print("Slow command: %v(%v) from client %v took %v\n", cmd, requestId, clientId, elapsed)
+	}
 }
 
 func (module *redisModule) onRequest(clientId, requestId int64, request [][]byte) {
+	startTime := time.Now()
 	response := newResponse(clientId, requestId)
 	go func() {
-		defer module.panicHandler(clientId, requestId, request[0])
+		defer module.commandCleanup(clientId, requestId, request[0], startTime)
 		ctx := context.WithValue(context.Background(), sessionIdKey, clientId)
 		module.worker.OnCommand(ctx, module.NewRedisClient(), request, response)
 		module.responses[clientId%int64(len(module.responses))].dispatch(response.serialize())
